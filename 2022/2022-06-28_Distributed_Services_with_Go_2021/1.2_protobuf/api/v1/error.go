eb081c80658626c7cea9726625348ed14c11afab
d2jvkpn <chenbin2018@protonmail.com>
2022-07-03 14:13:37 +0800

    ...

--- 2022/2022-06-28_Distributed_Services_with_Go_2021/1.2_protobuf/api/v1/error.go
+++ 2022/2022-06-28_Distributed_Services_with_Go_2021/1.2_protobuf/api/v1/error.go
@@ -11,19 +11,14 @@ type ErrOffsetOutOfRange struct {
 }
 
 func (e ErrOffsetOutOfRange) GRPCStatus() *status.Status {
-	st := status.New(
-		404,
-		fmt.Sprintf("offset out of range: %d", e.Offset),
-	)
+	st := status.New(404, fmt.Sprintf("offset out of range: %d", e.Offset))
+
 	msg := fmt.Sprintf(
 		"The requested offset is outside the log's range: %d",
 		e.Offset,
 	)
 
-	d := &errdetails.LocalizedMessage{
-		Locale:  "en-US",
-		Message: msg,
-	}
+	d := &errdetails.LocalizedMessage{Locale: "en-US", Message: msg}
 	std, err := st.WithDetails(d)
 	if err != nil {
 		return st
