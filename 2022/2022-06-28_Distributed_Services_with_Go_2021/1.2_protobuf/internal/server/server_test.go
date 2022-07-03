d55a6963c44c5f64c0423df206bd76b9478f0e8c
d2jvkpn <chenbin2018@protonmail.com>
2022-07-03 14:22:08 +0800

    ...

--- 2022/2022-06-28_Distributed_Services_with_Go_2021/1.2_protobuf/internal/server/server_test.go
+++ 2022/2022-06-28_Distributed_Services_with_Go_2021/1.2_protobuf/internal/server/server_test.go
@@ -70,15 +70,10 @@ func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
 	ctx := context.Background()
 
 	want := &api.Record{Value: []byte("hello world")}
-	produce, err := client.Produce(
-		ctx,
-		&api.ProduceRequest{Record: want},
-	)
+	produce, err := client.Produce(ctx, &api.ProduceRequest{Record: want})
 	require.NoError(t, err)
 
-	consume, err := client.Consume(ctx, &api.ConsumeRequest{
-		Offset: produce.Offset,
-	})
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: produce.Offset})
 	require.NoError(t, err)
 	require.Equal(t, want.Value, consume.Record.Value)
 	require.Equal(t, want.Offset, consume.Record.Offset)
