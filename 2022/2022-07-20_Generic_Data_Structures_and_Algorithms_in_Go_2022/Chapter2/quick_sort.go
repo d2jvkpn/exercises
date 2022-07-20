197e5d562ba3997b501e1d6127a884e7e3cc06bf
d2jvkpn <chenbin2018@protonmail.com>
2022-07-20 16:38:28 +0800

    ...

--- 2022/2022-07-20_Generic_Data_Structures_and_Algorithms_in_Go_2022/Chapter2/quick_sort.go
+++ 2022/2022-07-20_Generic_Data_Structures_and_Algorithms_in_Go_2022/Chapter2/quick_sort.go
@@ -10,13 +10,20 @@ type Ordered interface {
 
 func quicksort[T Ordered](data []T, low, high int) {
 	if low < high {
-		var pivot = partition(data, low, high)
-		quicksort(data, low, pivot)
-		quicksort(data, pivot+1, high)
+		var idx = partition(data, low, high)
+		fmt.Printf("    output: %v, pivotIndex: %d\n", data, idx)
+
+		if low < idx {
+			quicksort(data, low, idx)
+		}
+		if idx+1 < high {
+			quicksort(data, idx+1, high)
+		}
 	}
 }
 
 func partition[T Ordered](data []T, low, high int) int {
+	fmt.Printf("~~~ partition: %v, %d - %d\n", data, low, high)
 	// Pick a lowest bound element as a pivot value
 	var pivot = data[low]
 	var i, j = low, high
@@ -29,26 +36,33 @@ func partition[T Ordered](data []T, low, high int) int {
 			j--
 		}
 		if i < j {
-			b := fmt.Sprintf("%v", data)
+			fmt.Printf("    swap %d@%d, %d@%d, ", data[i], i, data[j], j)
 			data[i], data[j] = data[j], data[i]
-			fmt.Printf("~~~ 1: %v -> %v\n", b, data)
+			fmt.Println(data)
 		}
 	}
 
-	b := fmt.Sprintf("%v", data)
-	data[low], data[j] = data[j], pivot
-	fmt.Printf("~~~ 2: %v -> %v\n", b, data)
+	if j > low {
+		fmt.Printf("    swap %d@%d, %d@%d, ", data[low], low, data[j], j)
+		data[low], data[j] = data[j], pivot
+		fmt.Println(data)
+	}
 	return j
 }
 
 func main() {
-	numbers := []float64{3.5, -2.4, 12.8, 9.1, 4.5, 1.0}
-	fmt.Println(">>>", numbers)
-	quicksort[float64](numbers, 0, len(numbers)-1)
-	fmt.Println(numbers)
+	ints := []int64{7, 2, 1, 8, 6, 3, 5, 4}
+	fmt.Println(">>>", ints)
+	quicksort[int64](ints, 0, len(ints)-1)
+	fmt.Println(ints)
 
-	names := []string{"Zachary", "John", "Moe", "Jim", "Robert"}
-	fmt.Println(">>>", names)
-	quicksort[string](names, 0, len(names)-1)
-	fmt.Println(names)
+	//	numbers := []float64{3.5, -2.4, 12.8, 9.1, 4.5, 1.0}
+	//	fmt.Println(">>>", numbers)
+	//	quicksort[float64](numbers, 0, len(numbers)-1)
+	//	fmt.Println(numbers)
+
+	//	names := []string{"Zachary", "John", "Moe", "Jim", "Robert"}
+	//	fmt.Println(">>>", names)
+	//	quicksort[string](names, 0, len(names)-1)
+	//	fmt.Println(names)
 }
