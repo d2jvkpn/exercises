8aab18952d48f209d2731b13a6aadfa484fa6821
d2jvkpn <chenbin2018@protonmail.com>
2022-08-04 18:30:47 +0800

    ...

---
+++ 2022/2022-08-04_Back_Hat_Rust_2021/Chapter3/a01/src/main.rs
@@ -0,0 +1,89 @@
+use std::sync::Arc;
+use std::time::Duration;
+
+use chrono::{DateTime, Local, SecondsFormat};
+use futures::future::*;
+use tokio::spawn;
+use tokio::sync::{broadcast, mpsc, oneshot, Mutex};
+use tokio::time::sleep;
+
+async fn some_computation(input: u32) -> String {
+    sleep(Duration::from_millis(200)).await;
+    // "represents the result of the computation".to_string()
+
+    let now: DateTime<Local> = Local::now();
+    format!(
+        "~~~ {} the result of computation {}",
+        now.to_rfc3339_opts(SecondsFormat::Millis, true),
+        input,
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    // onshot
+    let (tx, rx) = oneshot::channel();
+
+    spawn(async move {
+        let res = some_computation(42).await;
+        tx.send(res).unwrap();
+    });
+    // Do other work while the computation is happening in the background
+    // Wait for the computation result
+
+    let res = rx.await.unwrap();
+    println!("~~~ oneshot result: {}", res);
+
+    // mpsc
+    let (tx, mut rx) = mpsc::channel(4);
+    //    tokio::spawn(async move {
+    //        for i in 0..16 {
+    //            let res = some_computation(i).await;
+    //            tx.send(res).await.unwrap();
+    //        }
+    //    });
+    for i in 0..16 {
+        let txc = tx.clone();
+        spawn(async move {
+            let res = some_computation(i).await;
+            txc.send(res).await.unwrap();
+        });
+    }
+    drop(tx);
+
+    while let Some(res) = rx.recv().await {
+        println!("{}", res);
+    }
+
+    // broadcast
+    let (tx, mut rx1) = broadcast::channel(16);
+    let mut rx2 = tx.subscribe();
+
+    let t1 = spawn(async move {
+        println!("~~~ rx1.recv: {}", rx1.recv().await.unwrap());
+        println!("~~~ rx1.recv: {}", rx1.recv().await.unwrap());
+    });
+
+    let t2 = spawn(async move {
+        println!("~~~ rx2.recv: {}", rx2.recv().await.unwrap());
+        println!("~~~ rx2.recv: {}", rx2.recv().await.unwrap());
+    });
+
+    tx.send(10).unwrap();
+    tx.send(20).unwrap();
+    join_all(vec![t1, t2]).await;
+
+    // Arc<Mutex<T>>
+    let data1 = Arc::new(Mutex::new(0));
+    let data2 = data1.clone(); // Arc::clone(&data1);
+
+    spawn(async move {
+        let mut value = data2.lock().await;
+        *value += 1;
+        println!("~~~ data a: {}", value);
+    });
+
+    let mut value = data1.lock().await;
+    *value += 1;
+    println!("~~~ data b: {}", value);
+}
