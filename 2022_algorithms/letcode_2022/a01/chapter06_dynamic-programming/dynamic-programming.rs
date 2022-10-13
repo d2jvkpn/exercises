b39a6f657a30beb9849ba1b5f530b2a063c70035
d2jvkpn <chenbin2018@protonmail.com>
2022-10-13 17:53:00 +0800

    ...

--- 2022_algorithms/letcode_2022/a01/chapter06_dynamic-programming/dynamic-programming.rs
+++ 2022_algorithms/letcode_2022/a01/chapter06_dynamic-programming/dynamic-programming.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 pub fn max_profit_v1(prices: &Vec<i32>) -> i32 {
     // let max_val = prices.iter().max();
 
@@ -11,13 +13,10 @@ pub fn max_profit_v1(prices: &Vec<i32>) -> i32 {
 
     let mut profit = 0;
 
-    prices[0..prices.len() - 1]
-        .iter()
-        .enumerate()
-        .for_each(|(idx, val)| {
-            let p = *prices[idx + 1..].iter().max().unwrap_or(&0) - val;
-            profit = if p > profit { p } else { profit };
-        });
+    prices[0..prices.len() - 1].iter().enumerate().for_each(|(idx, val)| {
+        let p = *prices[idx + 1..].iter().max().unwrap_or(&0) - val;
+        profit = if p > profit { p } else { profit };
+    });
 
     profit
 }
@@ -73,7 +72,90 @@ pub fn max_profit(prices: &Vec<i32>) -> i32 {
     return no_hold;
 }
 
+fn max_sub_array(nums: Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let max_val = |x1: i32, x2: i32| if x1 > x2 { x1 } else { x2 };
+
+    let (mut cur, mut max) = (nums[0], nums[0]);
+
+    nums[1..].iter().for_each(|x| {
+        cur = max_val(cur, 0) + *x;
+        max = max_val(max, cur);
+    });
+
+    return max;
+}
+
+pub fn rob(nums: Vec<i32>) -> i32 {
+    // todo!()
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let max_val = |x1: i32, x2: i32| if x1 > x2 { x1 } else { x2 };
+    let (mut dp0, mut dp1) = (0, nums[0]);
+
+    nums[1..].iter().for_each(|val| {
+        let temp = max_val(dp0, dp1);
+        dp1 = dp0 + *val;
+        dp0 = temp;
+    });
+
+    max_val(dp0, dp1)
+}
+
+fn climb_stairs_v1(n: i32) -> i32 {
+    if n <= 1 {
+        return 1;
+    }
+    if n < 3 {
+        return n;
+    }
+    println!("~~~ n={}", n);
+    climb_stairs_v1(n - 1) + climb_stairs_v1(n - 2)
+}
+
+fn climb_stairs_v2(n: i32) -> i32 {
+    // fibonacci, a: last climb is 1, b: last climb is 2
+    fn fib(n: i32, a: i32, b: i32) -> i32 {
+        if n <= 1 {
+            return b;
+        }
+        println!("~~~ n={}, a={}, b={}", n, a, b);
+        fib(n - 1, b, a + b)
+    }
+
+    fib(n, 1, 1)
+}
+
+fn climb_stairs(n: i32) -> i32 {
+    if n <= 1 {
+        return 1;
+    }
+
+    let len = n as usize;
+    let mut dp = vec![0; len + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for i in 3..=len {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    dp[len]
+}
+
 fn main() {
     let input = vec![7, 1, 5, 3, 6, 4];
-    println!("{}", max_profit(&input));
+    println!(">>> max_profit: {}", max_profit(&input));
+
+    println!(">>> max_sub_array: {:?}", max_sub_array(vec![5, 4, -1, 7, 8]));
+
+    println!(">>> rob: {:?}", rob(vec![2, 7, 9, 3, 1]));
+
+    // println!(">>> climb_stairs_v1: {:?}", climb_stairs_v1(45));
+    println!(">>> climb_stairs: {:?}", climb_stairs(45));
 }
