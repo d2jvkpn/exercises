0ab64e039286f80b67ccd5f893ba09641812e57a
d2jvkpn <chenbin2018@protonmail.com>
2022-11-10 11:59:15 +0800

    ...

--- 2022_courses/2022-11-09_Ultimate_Rust_2_Intermediate_Concepts_2022/x02/src/main.rs
+++ 2022_courses/2022-11-09_Ultimate_Rust_2_Intermediate_Concepts_2022/x02/src/main.rs
@@ -1,47 +1,4 @@
-#![allow(unused_imports)]
-
-use std::cmp::{Eq, PartialEq};
-use std::convert::{From, Into};
-use std::default::Default;
-use std::fmt;
-use std::marker::{Copy, Send, Sized, Sync, Unpin};
-
-#[derive(Clone, Debug)]
-pub struct Puzzle {
-    pub num_pieces: u32,
-    pub name: String,
-}
-
-#[derive(Clone, Copy)]
-pub enum PuzzleType {
-    Jigsaw,
-}
-
-impl fmt::Display for Puzzle {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "num_pieces={}, name={:?}", self.num_pieces, self.name)
-    }
-}
-
-impl Default for Puzzle {
-    fn default() -> Self {
-        Puzzle { num_pieces: 2022, name: "Unknown".into() }
-    }
-}
-
-impl From<&Puzzle> for u32 {
-    fn from(item: &Puzzle) -> Self {
-        item.num_pieces
-    }
-}
-
-impl PartialEq for Puzzle {
-    fn eq(&self, other: &Self) -> bool {
-        // todo!()
-        (self.num_pieces == other.num_pieces)
-            && (self.name.to_lowercase() == other.name.to_lowercase())
-    }
-}
+use x02::puzzle::Puzzle;
 
 fn main() {
     let puzzle = Puzzle { num_pieces: 42, name: "Forest Lake".into() };
@@ -53,6 +10,6 @@ fn main() {
         Puzzle { num_pieces: 24, ..Default::default() },
     );
 
-    let pieces: u32 = (&puzzle).into();
-    assert_eq!(pieces, u32::from(&puzzle));
+    let pieces: u16 = (&puzzle).into();
+    assert_eq!(pieces, u16::from(&puzzle));
 }
---
+++ 2022_courses/2022-11-09_Ultimate_Rust_2_Intermediate_Concepts_2022/x02/src/puzzle.rs
@@ -0,0 +1,59 @@
+#![allow(unused_imports)]
+
+use std::cmp::{Eq, PartialEq};
+use std::convert::{From, Into};
+use std::default::Default;
+use std::fmt;
+use std::marker::{Copy, Send, Sized, Sync, Unpin};
+
+#[derive(Clone, Debug)]
+pub struct Puzzle {
+    pub num_pieces: u16,
+    pub name: String,
+}
+
+#[derive(Clone, Copy)]
+pub enum PuzzleType {
+    Jigsaw,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum PuzzleError {
+    WontFit(u16),
+    MissingPieces,
+}
+
+impl fmt::Display for Puzzle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "num_pieces={}, name={:?}", self.num_pieces, self.name)
+    }
+}
+
+impl Default for Puzzle {
+    fn default() -> Self {
+        Puzzle { num_pieces: 2022, name: "Unknown".into() }
+    }
+}
+
+impl From<&Puzzle> for u16 {
+    fn from(item: &Puzzle) -> Self {
+        item.num_pieces
+    }
+}
+
+impl PartialEq for Puzzle {
+    fn eq(&self, other: &Self) -> bool {
+        // todo!()
+        (self.num_pieces == other.num_pieces)
+            && (self.name.to_lowercase() == other.name.to_lowercase())
+    }
+}
+
+impl fmt::Display for PuzzleError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::WontFit(v) => write!(f, "Piece {} doesn't fit!", v),
+            Self::MissingPieces => write!(f, "Missing a piece"),
+        }
+    }
+}
