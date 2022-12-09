ba3b8e287c3c0a7fb92242fd5c8ea3c1bcaf4a3a
d2jvkpn <chenbin2018@protonmail.com>
2022-12-09 08:50:12 +0800

    bubble_sort and select_sort

--- 2022_misc/substrate_homework/src/ch03.rs
+++ 2022_misc/substrate_homework/src/ch03.rs
@@ -8,6 +8,21 @@ pub fn bubble_sort<T: PartialOrd>(items: &mut Vec<T>) {
         return;
     }
 
+    for i in (1..len).rev() {
+        for j in 0..i {
+            if items[j] > items[j + 1] {
+                items.swap(j, j + 1)
+            }
+        }
+    }
+}
+
+pub fn select_sort<T: PartialOrd>(items: &mut Vec<T>) {
+    let len = items.len();
+    if len <= 1 {
+        return;
+    }
+
     for i in 0..len {
         for j in i + 1..len {
             if items[i] > items[j] {
@@ -19,12 +34,19 @@ pub fn bubble_sort<T: PartialOrd>(items: &mut Vec<T>) {
 
 #[cfg(test)]
 mod tests {
-    use super::bubble_sort;
+    use super::*;
 
     #[test]
     fn t_bubble_sort() {
         let mut items = vec![7, 3, 5, 1, 9];
         bubble_sort(&mut items);
+        assert_eq!(items, vec![1, 3, 5, 7, 9]);
+    }
+
+    #[test]
+    fn t_select_sort() {
+        let mut items = vec![7, 3, 5, 1, 9];
+        select_sort(&mut items);
         assert_eq!(items, vec![1, 3, 5, 7, 9]);
     }
 }
