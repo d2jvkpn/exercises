5351b1164163dd3ab4c6a0b357907acbd5160a04
d2jvkpn <chenbin2018@protonmail.com>
2023-01-12 17:37:43 +0800

    k8s autoscale

--- 2022_frameworks/k8s-on-kvm/2.install_k8s_01_auto.sh
+++ 2022_frameworks/k8s-on-kvm/2.install_k8s_01_auto.sh
@@ -69,7 +69,7 @@ sysctl --system
 mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |
-   sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+   gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
