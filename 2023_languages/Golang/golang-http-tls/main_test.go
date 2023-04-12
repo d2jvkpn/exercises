1b9a2617817f5304dfa3f9ddc3ef534483fb2f6a
d2jvkpn <chenbin2018@protonmail.com>
2023-04-12 21:56:03 +0800

    ...

---
+++ 2023_languages/Golang/golang-http-tls/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	addr := "https://localhost:8443"
+
+	client := new(http.Client)
+	client.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	req, _ := http.NewRequest("GET", addr, nil)
+	// req.WithContext(ctx)
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bts, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("~~~ response: %q\n", bts)
+}
