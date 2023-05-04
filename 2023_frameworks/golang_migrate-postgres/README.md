d8d10518ad858a9f9bd1a42b4cd00a685b40a6d4
d2jvkpn <chenbin2018@protonmail.com>
2023-05-04 11:34:21 +0800

    add 2023_frameworks/golang_migrate-postgres

---
+++ 2023_frameworks/golang_migrate-postgres/README.md
@@ -0,0 +1,28 @@
+### Title
+---
+
+*https://dev.to/techschoolguru/how-to-write-run-database-migration-in-golang-5h6g*
+
+#### Installation
+```bash
+go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+
+go install github.com/mikefarah/yq/v4@latest
+```
+
+#### Create Makefile
+...
+
+
+#### Migration
+```bash
+migrate create -ext sql -dir migrations -seq init_schema
+
+echo "export DATABASE_URL=postgresql://hello:world@localhost:5433/simple_bank?sslmode=disable" > .env
+
+. .env
+
+migrate -path migrations -database "$DATABASE_URL" -verbose up
+
+migrate -path migrations -database "$DATABASE_URL" -verbose down -all
+```
---
+++ 2023_frameworks/golang_migrate-postgres/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	// _ "github.com/lib/pq"
+)
+
+func main() {
+	err := MigratePostgres(
+		"postgresql://hello:world@localhost:5433/simple_bank?sslmode=disable",
+		"./migrations",
+	)
+
+	if err != nil {
+		log.Fatalln("!!! ", err)
+	} else {
+		log.Println("==> Migration is ok")
+	}
+}
+
+func MigratePostgres(source, migrations string) (err error) {
+	var (
+		db     *sql.DB
+		driver database.Driver
+		migr   *migrate.Migrate
+	)
+
+	if db, err = sql.Open("postgres", source); err != nil {
+		return fmt.Errorf("sql.Open: %w", err)
+	}
+
+	if driver, err = postgres.WithInstance(db, &postgres.Config{}); err != nil {
+		return fmt.Errorf("postgres.WithInstance: %w", err)
+	}
+
+	migr, err = migrate.NewWithDatabaseInstance(
+		fmt.Sprintf("file://%s", migrations),
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("migrate.NewWithDatabaseInstance: %w", err)
+	}
+
+	// or m.Step(2) if you want to explicitly set the number of migrations to run
+	if err = migr.Up(); err != nil {
+		if err.Error() != "no change" {
+			return fmt.Errorf("migrate.Up: %w", err)
+		}
+	}
+
+	_, _ = migr.Close()
+	return nil
+}
