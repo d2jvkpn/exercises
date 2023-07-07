e691a2e6f7c8dd11ef6398c7c2180ec56d7851e2
d2jvkpn <chenbin2018@protonmail.com>
2023-07-07 09:47:38 +0800

    2023-07-06_The_Beginner_s_guide_to_Advanced_C_coding_in_Linux_2023

---
+++ 2023_courses/2023-07-06_The_Beginner_s_guide_to_Advanced_C_coding_in_Linux_2023/ch01/user_input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char name[6];
+    printf("==> What's your name? ");
+    scanf("%s", name);
+    printf("~~~ Hello, %s.\n", name);
+
+    int num;
+    printf("==> Your age: ");
+    scanf("%d", &num);
+    printf("~~~ Age: %d.\n", num);
+
+    float pi, r, ans;
+    pi = 3.14;
+    printf("==> What is R in cm? ");
+    scanf("%f", &r);
+    ans = r * r * pi;
+    printf("~~~ The area is %f\n", ans);
+
+    return 0;
+}
