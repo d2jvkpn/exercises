00e6b6650d9e1f92c77688b9f70d68d824a86773
d2jvkpn <chenbin2018@protonmail.com>
2023-10-20 09:34:26 +0800

    ...

---
+++ 2023_algorithms/2023-10-19_Data-Structures-and-Algorithms-Specialization-2023/ch01/greatest_common_divisor.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+/*
+gcd(a, b) = gcd(a', b) = gcd(a, b')
+a' + b*n = a
+n*a + b' = b
+
+3918848, 1653264
+*/
+fn main() {
+    let mut input = String::new();
+
+    println!("Enter two numbers:");
+
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let a: usize = input.trim().parse().expect("Input not an integer");
+
+    input.clear();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let b: usize = input.trim().parse().expect("Input not an integer");
+
+    println!("ans: {}", greatest_common_divisor(a, b));
+}
+
+fn greatest_common_divisor(a: usize, b: usize) -> usize {
+    println!("~~~ a={a}, b={b}");
+    if b == 0 {
+        return a;
+    }
+
+    return greatest_common_divisor(b, a % b);
+}
+
+#[cfg(tests)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t_gcd() {
+        assert_eq!(greatest_common_divisor(3918848, 1653264), 61232);
+    }
+}
