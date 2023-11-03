de26409da20ec259bb09b66247919064f097af18
d2jvkpn <chenbin2018@protonmail.com>
2023-11-03 12:02:14 +0800

    ...

--- 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c1501_linked_list.cpp
+++ 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c1501_linked_list.cpp
@@ -5,16 +5,20 @@ using namespace std;
 template <typename T>
 class Node {
 public:
-	T     data;
+	T        value;
 	Node<T>* next;
 
-	Node(T data) {
-		this->data = data;
+	Node(T value) {
+		this->value = value;
 		this->next = NULL;
 	}
 
+	~Node() {
+		cout << "!!! delete node: " << value << endl;
+	}
+
 	void show() {
-		cout << "Node(" << this->data << ")\n";
+		cout << "Node(" << this->value << ")\n";
 	}
 };
 
@@ -33,6 +37,19 @@ public:
 
 	~List() {
 		// TODO:
+		cout << "!!! DELETE List: " << this->size << endl;
+
+		this->size = 0;
+		this->tail = NULL;
+
+		Node<T>* next = head;
+		Node<T>* temp;
+
+		while (next != NULL) {
+			temp = next -> next;
+			delete next;
+			next = temp;
+		};
 	}
 
 	void show() {
@@ -45,7 +62,7 @@ public:
 		Node<T>* next = head;
 
 		while(true) {
-			cout << ", " << next->data;
+			cout << ", " << next->value;
 
 			if (next->next == NULL) {
 				break;
@@ -57,8 +74,8 @@ public:
 		cout << ")\n";
 	}
 
-	void push_front(T data) {
-		Node<T>*node = new Node(data);
+	void push_front(T value) {
+		Node<T>*node = new Node(value);
 		this->size += 1;
 
 		if (head == NULL) {
@@ -70,8 +87,8 @@ public:
 		}
 	}
 
-	void push_back(T data) {
-		Node<T>*node = new Node(data);
+	void push_back(T value) {
+		Node<T>*node = new Node(value);
 		this->size += 1;
 
 		if (head == NULL) {
@@ -83,19 +100,19 @@ public:
 		}
 	}
 
-	void insert(T data, int pos) {
+	void insert(T value, int pos) {
 		// assert!(pos >= 0)
 		this->size += 1;
 
 		if (size == 0 || pos == 0) {
-			push_front(data);
+			push_front(value);
 			return;
 		} else if (pos >= size-1) {
-			push_back(data);
+			push_back(value);
 			return;
 		}
 
-		Node<T>* node = new Node(data);
+		Node<T>* node = new Node(value);
 		Node<T>* next = head;
 
 		for (int i=0; i<pos-1; i++) {
@@ -104,6 +121,131 @@ public:
 
 		node->next = next->next;
 		next->next = node;
+	}
+
+	int search(T value) {
+		Node<T>* next = head;
+		int i= 0;
+
+		while (next != NULL) {
+			if (next->value == value) {
+				return i;
+			}
+
+			next = next->next;
+			i+=1;
+		}
+
+		return -1;
+	}
+
+	bool remove(T value) {
+		if (this->size == 0) {
+			return false;
+		}
+
+		Node<T>* next = head;
+		Node<T>* parent;
+
+		while (next != NULL) {
+			if (next->value == value) {
+				break;
+			}
+
+			parent = next;
+			next = next -> next;
+		}
+
+		if (next == NULL) {
+			return false;
+		}
+
+		this->size -= 1;
+		if (this->size == 0) { // parent == NULL
+			this->head = NULL;
+			this->tail = NULL;
+		} else {
+			parent->next = next->next;
+		}
+
+		if (parent->next == NULL) {
+			this->tail = parent;
+		}
+
+		next->next = NULL;
+
+		return true;
+	}
+
+	Node<T>* pop_back() {
+		Node<T>* ans;
+
+		if (this->size == 0) {
+			return NULL;
+		}
+
+		ans = this->tail;
+		this->size -= 1;
+
+		if (this->size == 0) {
+			this->head = NULL;
+			this->tail = NULL;
+		} else {
+			Node<T>* next = this->head;
+
+			while (next->next != this->tail) {
+				next = next->next;
+			}
+
+			this->tail = next;
+		}
+
+		ans->next = NULL;
+
+		return ans;
+	}
+
+	Node<T>* pop_front() {
+		Node<T>* ans;
+
+		if (this->size == 0) {
+			return NULL;
+		}
+
+		ans = this->head;
+		this->size -= 1;
+
+		if (this->size == 0) {
+			this->head = NULL;
+			this->tail = NULL;
+		} else {
+			this->head = this->head->next;
+		}
+
+		ans->next = NULL;
+
+		return ans;
+	}
+
+	void reverse() {
+		if (this->size <= 1) {
+			return;
+		}
+
+		Node<T>* current = head;
+		Node<T>* prev = NULL;
+		Node<T>* temp;
+
+		while (current != NULL) {
+			temp = current->next;
+			current->next = prev;
+
+			prev = current;
+			current = temp;
+		}
+
+		tail = head;
+		head = prev;
 	}
 };
 
@@ -119,9 +261,17 @@ int main() {
 
 	list.push_back(102);
 	list.push_back(103);
+	list.push_front(41);
 	list.show();
 
 	list.insert(199, 2);
+	list.show();
+
+	cout << "==> Search 33: " << list.search(33) << endl;
+	cout << "==> Search 199: " << list.search(199) << endl;
+
+	list.show();
+	list.reverse();
 	list.show();
 
 	return 0;
