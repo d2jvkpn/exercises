6436891589eeb843c7983a722865440fae5799ba
d2jvkpn <chenbin2018@protonmail.com>
2023-11-08 02:10:30 +0800

    ...

--- 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c1801_binary_tree.cpp
+++ 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c1801_binary_tree.cpp
@@ -1,22 +1,11 @@
 # include <iostream>
 # include <string>
+# include "c1801_node.h"
 
 using namespace std;
 
-class Node {
-public:
-	char  data;
-	Node* left;
-	Node* right;
-
-	Node(int data) {
-		this->data = data;
-		left = NULL;
-		right = NULL;
-	}
-};
-
 // string = to_string(int)
+// 
 Node* buildNode(string msg) {
 	char data;
 
@@ -38,10 +27,59 @@ Node* buildNode(string msg) {
 	return node;
 }
 
+class Tree {
+public:
+	Node* root;
+
+	Tree() {
+		root = NULL;
+	}
+
+	~Tree() {
+		if (root != NULL) {
+			delete root;
+			root = NULL;
+		}
+
+		cout << "!!! delete Tree" << endl;
+	}
+
+	void build() {
+		Node* root = buildNode("root node(a-z)");
+		this->root = root;
+	}
+
+	void show() {
+		if (root == NULL) {
+			cout << "Tree{ root: NULL }" << endl;
+		}
+
+		string s1, s2;
+
+		s1 = (root->left == NULL) ? "NULL" : s1 + root->left->data;
+		s2 = (root->right == NULL) ? "NULL" : s2 + root->right->data;
+
+		cout << "Tree{ root: '" << root->data;
+		cout << "', left: " << s1 << ", right: " << s2 << "}" << endl;
+	}
+};
+
 int main() {
 	// cout << "Hello, world!\n";
 
-	Node* node = buildNode("root");
+	Node* node = new Node('a');
+	node->left = new Node('b');
+	node->show();
+	node->right = new Node('c');
+
+	delete node;
+
+	//
+	Tree* tree = new Tree;
+	tree->build();
+	tree->show();
+
+	delete tree;
 
 	return 0;
 }
