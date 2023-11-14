1273095f862d714a001ae6835a5e92aa4f61115c
evol <chenbin2018@protonmail.com>
2023-11-14 18:40:47 +0800

    ...

--- 2023_algorithms/rust/basics/src/tree/binary_tree.rs
+++ 2023_algorithms/rust/basics/src/tree/binary_tree.rs
@@ -4,7 +4,7 @@ use std::{cmp::PartialOrd, fmt::Debug};
 
 #[derive(Debug)]
 pub struct Tree<T> {
-    root: Child<T>,
+    pub root: Child<T>,
     size: usize,
 }
 
@@ -16,6 +16,12 @@ impl<T: Clone + Debug + PartialEq + PartialOrd> Tree<T> {
     pub fn clear(&mut self) {
         _ = self.root.take();
         self.size = 0;
+    }
+
+    pub fn root_value(&self) -> Option<T> {
+        let node = self.root.clone()?;
+        let ans = Some(node.borrow().data.clone());
+        ans
     }
 
     pub fn push_iter(item: &Child<T>, node: Node<T>) {
@@ -346,95 +352,5 @@ pub fn push_recur<T: PartialOrd>(parent: &mut Node<T>, node: Node<T>) {
         parent.right = Some(v);
     } else {
         parent.right = node.into();
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::tree::traversal::*;
-
-    #[test]
-    fn t1_binary_tree() {
-        let mut tree = Tree::new();
-        tree.root = Node::new(10).into();
-        println!("tree: {:?}", tree);
-
-        tree.push(5).push(1);
-        tree.push(12);
-        tree.push(4).push(6).push(8);
-
-        assert_eq!(tree.count(), 7);
-        assert!(tree.remove(1));
-        assert!(!tree.remove(13));
-        assert_eq!(tree.count(), 6);
-        println!("tree: {:?}", tree);
-
-        println!("tree.bfs: {:?}", tree.bfs());
-        assert_eq!(tree.bfs(), vec![10, 5, 12, 4, 6, 8]);
-    }
-
-    #[test]
-    fn t2_binary_tree() {
-        let mut tree: Tree<usize>;
-        let mut slice: &[usize];
-        tree = Tree::new();
-
-        // tree.push(8).push(3).push(10).push(1).push(6).push(14).push(4).push(7).push(13).push(19);
-        tree.clear();
-        slice = &[8, 3, 10, 1, 6, 14, 4, 7, 13, 19];
-        tree.push_slice(slice);
-        // println!("==> bfs: {:?}", tree.bfs());
-        assert_eq!(tree.bfs(), vec![8, 3, 10, 1, 6, 14, 4, 7, 13, 19]);
-        assert_eq!(tree.get_range(10, 30), vec![10, 13, 14, 19]);
-
-        print!("==> inorder_do print: ");
-        tree.inorder_do(|v: &usize| print!("{}, ", v));
-        println!("");
-
-        assert_eq!(tree.count(), slice.len());
-        assert_eq!(tree.root.clone().unwrap().borrow().data, 8);
-
-        tree.remove(8);
-        assert_eq!(tree.count(), slice.len() - 1);
-        assert_eq!(tree.root.clone().unwrap().borrow().data, 10);
-        assert_eq!(inorder_recur_a(&tree.root), vec![1, 3, 4, 6, 7, 10, 13, 14, 19]);
-        // println!("==> 1. {:?}, remove {}, inorder: {:?}", slice, 8, inorder_recur_a(&tree.root));
-
-        tree.clear();
-        slice = &[8, 10, 14];
-        tree.push_slice(slice);
-        tree.remove(8);
-        assert_eq!(tree.count(), slice.len() - 1);
-        assert_eq!(tree.root.clone().unwrap().borrow().data, 10);
-        assert_eq!(inorder_recur_a(&tree.root), vec![10, 14]);
-        // println!("==> 2. {:?}, remove {}, inorder: {:?}", slice, 8, inorder_recur_a(&tree.root));
-
-        tree.clear();
-        slice = &[8, 10, 14];
-        tree.push_slice(slice);
-        tree.remove(10);
-        assert_eq!(tree.count(), slice.len() - 1);
-        assert_eq!(tree.root.clone().unwrap().borrow().data, 8);
-        assert_eq!(inorder_recur_a(&tree.root), vec![8, 14]);
-        // println!("==> 3. {:?}, remove {}, inorder: {:?}", slice, 10, inorder_recur_a(&tree.root));
-
-        tree.clear();
-        slice = &[8];
-        tree.push_slice(slice);
-        tree.remove(8);
-        assert_eq!(tree.count(), slice.len() - 1);
-        assert!(tree.root.is_none());
-        assert_eq!(inorder_recur_a(&tree.root), vec![]);
-        // println!("==> 4. {:?}, remove {}, inorder: {:?}", slice, 8, inorder_recur_a(&tree.root));
-
-        tree.clear();
-        slice = &[8, 3];
-        tree.push_slice(slice);
-        tree.remove(8);
-        assert_eq!(tree.count(), slice.len() - 1);
-        assert_eq!(tree.root.clone().unwrap().borrow().data, 3);
-        assert_eq!(inorder_recur_a(&tree.root), vec![3]);
-        // println!("==> 5. {:?}, remove {}, inorder: {:?}", slice, 8, inorder_recur_a(&tree.root));
     }
 }
--- 2023_algorithms/rust/basics/src/tree/mod.rs
+++ 2023_algorithms/rust/basics/src/tree/mod.rs
@@ -4,3 +4,5 @@ pub mod binary_tree;
 pub mod node;
 pub mod traversal;
 pub mod tree;
+
+mod binary_tree_test;
