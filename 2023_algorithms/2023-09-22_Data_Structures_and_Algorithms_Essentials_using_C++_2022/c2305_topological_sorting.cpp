8f7707f4ec40b8075e4f077630759cd9bb6beb5b
evol <chenbin2018@protonmail.com>
2023-11-28 17:58:00 +0800

    ...

--- 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c2305_topological_sorting.cpp
+++ 2023_algorithms/2023-09-22_Data_Structures_and_Algorithms_Essentials_using_C++_2022/c2305_topological_sorting.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 template <typename T>
 class Graph {
+	string            name;
 	int               size;
 	vector<vector<T>> data;
 
 public:
-	Graph(int size) {
+	Graph(string name, int size) {
+		this->name = name;
 		this->size = size;
 
 		vector<T> item;
@@ -25,10 +27,11 @@ public:
 	}
 
 	~Graph() {
-		cout << "!!! delete graph" << endl;
+		cout << "!!! delete graph: " << name << ", size=" << size << endl;
 	}
 
-	bool addEdge(int i, int j, bool undirected=true) {
+	// bool addEdge(int i, int j, bool undirected=true) {
+	bool addEdge(int i, int j, bool undirected) {
 		if (i >= size || j >= size) {
 			return false;
 		}
@@ -99,23 +102,72 @@ public:
 
 		cout << "  CALL: " << node << endl;
 	}
+
+	void topological_sort() {
+		cout << "==> Topological Sort:" << endl;
+		vector<int> indegree(this->size, 0);
+
+		for (int i=0; i<this->size; i++) {
+			for (auto nbr: this->data[i]) {
+				indegree[nbr]++;
+			}
+		}
+
+		cout << "--> indegree:" << endl;
+		for (int i=0; i<this->size; i++) {
+			cout << "  " << i << ": " << indegree[i] << endl;
+		}
+
+		queue<int> q;
+		for (int i=0; i<this->size; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+				cout << "  push: " << i << endl;
+			}
+		}
+
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+
+			cout << "  CALL: " << node << endl;
+			for (auto nbr: this->data[node]) {
+				indegree[nbr]--;
+				if (indegree[nbr] == 0) {
+					q.push(nbr);
+					cout << "  push: " << nbr << endl;
+				}
+			}
+		}
+		cout << endl;
+	}
 };
 
 int main() {
 	// cout << "Hello, world!\n";
 
-	Graph<int> graph(7);
-	graph.addEdge(0, 1);
-	graph.addEdge(1, 2);
-	graph.addEdge(3, 5);
-	graph.addEdge(5, 6);
-	graph.addEdge(4, 5);
-	graph.addEdge(0, 4);
-	graph.addEdge(3, 4);
+	Graph<int> g1("g1", 7);
+	g1.addEdge(0, 1, true);
+	g1.addEdge(1, 2, true);
+	g1.addEdge(3, 5, true);
+	g1.addEdge(5, 6, true);
+	g1.addEdge(4, 5, true);
+	g1.addEdge(0, 4, true);
+	g1.addEdge(3, 4, true);
 
-	graph.show();
-    graph.bfs(1);
-    graph.dfs(1);
+	g1.show();
+    g1.bfs(1);
+    g1.dfs(1);
+
+	Graph<int> g2("g2", 6);
+	g2.addEdge(0, 2, false);
+	g2.addEdge(2, 3, false);
+	g2.addEdge(3, 5, false);
+	g2.addEdge(4, 5, false);
+	g2.addEdge(1, 4, false);
+	g2.addEdge(1, 2, false);
+
+	g2.topological_sort();
 
 	return 0;
 }
