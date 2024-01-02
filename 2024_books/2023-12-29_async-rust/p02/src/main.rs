d8632354402900cf8bb5fa6cd057fffb025c692c
d2jvkpn <chenbin2018@protonmail.com>
2024-01-03 00:13:46 +0800

    ...

--- 2024_books/2023-12-29_async-rust/p02/src/main.rs
+++ 2024_books/2023-12-29_async-rust/p02/src/main.rs
@@ -1,5 +1,7 @@
+#![allow(unused_imports)]
+
 use std::{
-    fs::{File, OpenOptions},
+    fs::{create_dir_all, File, OpenOptions},
     future::Future,
     io::prelude::*,
     path::Path,
@@ -8,42 +10,48 @@ use std::{
     task::{Context, Poll},
 };
 
+use chrono::{Local, SecondsFormat};
 use futures_util::future::join_all;
-use tokio::task::{JoinHandle, spawn};
+use tokio::task::{spawn, JoinHandle};
 
 type AsyncFileHandle = Arc<Mutex<File>>;
-type FileJoinHandle = JoinHandle<Result<bool, String>>;
+// type FileJoinHandle = JoinHandle<Result<bool, String>>;
 
 #[tokio::main]
 async fn main() {
     // println!("Hello, world!");
 
-	let login_handle = get_handle(&"login.txt");
-	let logout_handle = get_handle(&"logout.txt");
+    let login_handle = get_handle(&"logs/login.txt");
+    let logout_handle = get_handle(&"logs/logout.txt");
 
-	let names = ["one", "two", "three", "four", "five", "six"];
+    let names = ["one", "two", "three", "four", "five", "six"];
 
-	let mut handles = Vec::new();
+    let mut handles = Vec::new();
 
-	for name in names {
-		let handle1 = login_handle.clone();
-		let handle2 = logout_handle.clone();
+    for name in names {
+        let handle1 = login_handle.clone();
+        let handle2 = logout_handle.clone();
 
-		let handle1 = write_log(handle1, name.to_string());
-		let handle2 = write_log(handle2, name.to_string());
+        let handle1 = write_log(handle1, name.to_string());
+        let handle2 = write_log(handle2, name.to_string());
 
-		handles.push(handle1);
-		handles.push(handle2);
-	}
+        handles.push(handle1);
+        handles.push(handle2);
+    }
 
-	let _ = join_all(handles).await;
+    let _ = join_all(handles).await;
 }
 
 fn get_handle(fp: &str) -> AsyncFileHandle {
     match OpenOptions::new().append(true).open(fp) {
-        Ok(v) => Arc::new(Mutex::new(v)),
-        Err(_) => Arc::new(Mutex::new(File::create(fp).unwrap())),
+        Ok(v) => return Arc::new(Mutex::new(v)),
+        Err(_) => {}
     }
+
+    let path = Path::new(fp).parent().unwrap();
+    create_dir_all(path).unwrap();
+
+    return Arc::new(Mutex::new(File::create(fp).unwrap()));
 }
 
 struct AsyncWriteFuture {
@@ -64,14 +72,18 @@ impl Future for AsyncWriteFuture {
             }
         };
 
-        let lined_entry = format!("{}\n", self.entry);
+        let lined_entry = format!(
+            "{} {}\n",
+            Local::now().to_rfc3339_opts(SecondsFormat::Millis, false),
+            self.entry,
+        );
 
         match guard.write_all(lined_entry.as_bytes()) {
-            Ok(_) => println!("~~~ written for: {}", lined_entry),
+            Ok(_) => print!("~~~ written for: {}", lined_entry),
             Err(e) => {
-            	println!("!!! {}", e);
-            	ctx.waker().wake_by_ref();
-            	return Poll::Pending;
+                println!("!!! {}", e);
+                ctx.waker().wake_by_ref();
+                return Poll::Pending;
             }
         };
 
@@ -80,12 +92,11 @@ impl Future for AsyncWriteFuture {
 }
 
 async fn write_log(file_handle: AsyncFileHandle, line: String) {
-	let future = AsyncWriteFuture{
-		handle: file_handle,
-		entry: line,
-	};
+    let future = AsyncWriteFuture {
+        handle: file_handle,
+        entry: line,
+    };
 
-	spawn(async move {
-		future.await
-	});
+    // spawn(async move { future.await });
+    let _ = future.await;
 }
