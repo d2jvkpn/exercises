41083713151ef1026e20a2e01c3529ff1309a093
evol <chenbin2018@protonmail.com>
2024-02-02 14:06:52 +0800

    ...

--- 2024_courses/Lets_Get_Rusty/a03_structs/string.rs
+++ 2024_courses/Lets_Get_Rusty/a03_structs/string.rs
@@ -1,4 +1,7 @@
+#![allow(unused_variables)]
+
 fn main() {
+    //
     let tweet = String::from("This is my tweet and it's very very long.");
 
     let trimed_tweet = trim_tweet(&tweet); // pass &String, derefrence conversion
@@ -19,9 +22,58 @@ fn main() {
     println!("~~~ get_chars(hello_str, 3): {:?}", get_chars(hello_str, 3));
     println!(r#"~~~ get_chars("你好"， 20): {:?}"#, get_chars("你好", 24));
 
+    //
     let a = [1, 2, 3, 4, 5, 6];
     let a_slice = &a[..3];
     println!("~~~ a_slice: {a_slice:?}");
+
+    //
+    let s1: &str = "Hello, world!";
+    let s2: String = String::from("Hello, world!");
+    let s3: String = "Hello, world!".to_string();
+    let s4: String = "Hello, world!".to_owned();
+    let s5: &str = &s4[..];
+    let s6: String = "Hello, world!".into();
+
+    //
+    let mut s: String = String::from("foo");
+    s.push_str(" bar");
+    dbg!(&s);
+    s.replace_range(.., "baz");
+    dbg!(&s);
+
+    //
+    let s1 = String::from("Hello");
+    let s2 = String::from("world");
+    let s3 = s1 + ", " + &s2; // s1 was removed
+    dbg!((&s2, &s3));
+
+    let s1 = ["frist", "second"].concat();
+    let s2 = format!("{} {}", "first", "second");
+    let s3 = concat!("first", ", ", "second");
+    dbg!((&s1, &s2, &s3));
+
+    //
+    let s1: &str = "你a好，Rust 编程语言！";
+    println!(
+        "~~~ len(s1): {}, s1.bytes().count(): {}, s1.chars().count(): {}",
+        s1.len(),
+        s1.bytes().count(),
+        s1.chars().count()
+    );
+    println!("~~~ s1[0..3]: {}, s1[3..4]: {}", &s1[0..3], &s1[3..4]);
+
+    println!("~~~ bytes:");
+    for b in s1.bytes() {
+        print!("{b}, ")
+    }
+    println!("");
+
+    println!("~~~ chars:");
+    for c in s1.chars() {
+        print!("'{c}', ")
+    }
+    println!("")
 }
 
 // fn trim_tweet(tweet: &String) -> &str {
---
+++ 2024_courses/Lets_Get_Rusty/a03_structs/structs.rs
@@ -0,0 +1,135 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::fmt;
+
+fn main() {
+    // structs
+    // let product = Product::new("Hello".to_string(), 42.0).with_in_stack(true);
+
+    let mut product: Product = Default::default();
+    product.set_name("Hello").set_price(42.25).set_in_stack(true);
+
+    println!("~~~ {}", product);
+    let num = 3;
+    println!("==> You need to pay ${} for {} {:?}.", product.pay(num), num, product.name);
+
+    // tuples structs
+    let rbg_color: (i32, i32, i32) = (256, 106, 0);
+    let cmyk_color: (i32, i32, i32, i32) = (0, 58, 100, 0);
+
+    #[derive(Debug)]
+    struct RGB(i32, i32, i32);
+
+    #[derive(Debug)]
+    struct CMYK(i32, i32, i32, i32);
+
+    let color1 = RGB(256, 106, 0);
+    let color2 = CMYK(0, 58, 100, 0);
+    println!("~~~ color1: {:?}, color2: {color2:?}", color1);
+
+    // unit-like structs
+    struct MyStruct;
+
+    // enum
+    let category = Category::Clothing;
+    dbg!(&category);
+
+    let cmd = Command::Undo;
+    let cmd = Command::AddText(String::from("Test"));
+    let cmd = Command::Replace { from: "a".to_string(), to: "b".to_string() };
+
+    println!("==> cmd: {:?}, serilize: {}", cmd, cmd.serialize());
+}
+
+#[derive(Debug)]
+struct Product {
+    name: String,
+    price: f32,
+    in_stock: bool,
+}
+
+impl Product {
+    pub fn new(name: String, price: f32) -> Self {
+        Self { name, price, in_stock: false }
+    }
+
+    pub fn default_sales_tax() -> f32 {
+        0.1
+    }
+
+    pub fn with_in_stack(mut self, in_stack: bool) -> Self {
+        self.in_stock = in_stack;
+        self
+    }
+
+    pub fn set_name(&mut self, name: &str) -> &mut Self {
+        self.name = name.to_string();
+        self
+    }
+
+    pub fn set_price(&mut self, price: f32) -> &mut Self {
+        self.price = price;
+        self
+    }
+
+    pub fn set_in_stack(&mut self, in_stock: bool) -> &mut Self {
+        self.in_stock = in_stock;
+        self
+    }
+
+    pub fn sales_tax(&self) -> f32 {
+        self.price * Self::default_sales_tax()
+    }
+
+    pub fn pay(&self, num: usize) -> f32 {
+        self.price * (num as f32)
+    }
+}
+
+impl fmt::Display for Product {
+    fn fmt(&self, w: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            w,
+            "Product {{name: {:?}, price: {}, in_stock: {}}}",
+            self.name, self.price, self.in_stock,
+        )
+    }
+}
+
+impl Default for Product {
+    fn default() -> Self {
+        Self { name: "".to_string(), price: 0.0, in_stock: false }
+    }
+}
+
+#[derive(Debug)]
+enum Category {
+    Book,
+    Clothing,
+    Electrics,
+    Other,
+}
+
+#[derive(Debug)]
+enum Command {
+    Undo,
+    Redo,
+    AddText(String),
+    MoveCursor(i32, i32),
+    Replace { from: String, to: String },
+}
+
+impl Command {
+    fn serialize(&self) -> String {
+        match self {
+            Self::Undo => "undo".into(),
+            Self::Redo => "redo".into(),
+            Self::AddText(v) => format!("add_text({:?})", v),
+            Self::MoveCursor(ref x, ref y) => format!("move_coursor({}, {})", x, y),
+            Self::Replace { ref from, to } => {
+                format!("replace{{from: {:?}, to: {:?}}}", from, to)
+            }
+        }
+    }
+}
