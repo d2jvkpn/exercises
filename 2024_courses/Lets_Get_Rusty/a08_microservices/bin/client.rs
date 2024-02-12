e40352eb3e70b55e99c0fefba78befa92c4041ff
d2jvkpn <chenbin2018@protonmail.com>
2024-02-12 15:23:49 +0800

    ...

--- 2024_courses/Lets_Get_Rusty/a08_microservices/bin/client.rs
+++ 2024_courses/Lets_Get_Rusty/a08_microservices/bin/client.rs
@@ -1,7 +1,9 @@
-pub mod hello {
+use tonic::Request;
+
+pub mod hello_proto {
     tonic::include_proto!("hello");
 }
-use hello::{greeter_client::GreeterClient, HelloRequest};
+use hello_proto::{greeter_client::GreeterClient, HelloRequest};
 
 use std::error::Error;
 
@@ -9,7 +11,7 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut client = GreeterClient::connect("http://127.0.0.1:50001").await?;
 
-    let request = tonic::Request::new(HelloRequest { name: "Tonic".into() });
+    let request = Request::new(HelloRequest { name: "Tonic".into() });
 
     let response = client.say_hello(request).await?;
     let response = response.into_inner();
