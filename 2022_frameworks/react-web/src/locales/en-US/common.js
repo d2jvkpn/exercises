bb95f52ea51ae6852c2ed7b1e5a19aeb8868f81d
d2jvkpn <chenbin2018@protonmail.com>
2024-04-01 20:19:01 +0800

    move react-web to playground/docker/react-web

--- 2022_frameworks/react-web/src/locales/en-US/common.js
+++
@@ -1,7 +0,0 @@
-let data = {
-  hello: "Hello",
-  world: "world",
-  welcome: "~~~ Welcome, %1$s...",
-};
-
-export default data;
--- 2022_frameworks/react-web/src/locales/en-US/index.js
+++
@@ -1,7 +0,0 @@
-import common from './common.js';
-
-let data = {
-  common: common,
-};
-
-export default data;
