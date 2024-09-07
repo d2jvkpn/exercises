aa81fc6ab6c5ca1433de659fc074ae7440b055fe
d2jvkpn <chenbin2018@protonmail.com>
2024-09-07 10:34:10 +0800

    ...

--- 2024_books/Rust_Advanced_Techniques/ch02/c2-1-4/src/main.rs
+++ 2024_books/Rust_Advanced_Techniques/ch02/c2-1-4/src/main.rs
@@ -6,6 +6,7 @@ use std::marker::PhantomData;
 fn main() {
 	println!("Hello, world!");
 
+	// 1.
 	let my_poodle: Dog<Poodle> = Dog { name: "Jeffrey".into(), breed: PhantomData };
 	// println!("{}", my_poodle.breed_name()); // Note: compile error
 
@@ -15,8 +16,16 @@ fn main() {
 
 	let my_labrador: Dog<Labrador> = Dog::new("Jeffrey".to_string());
 	println!("==> My dog is a {}, named {}.", my_labrador.breed_name(), my_labrador.name);
+
+	// 2.
+	let cat = Cat;
+	println!("==> I am a {}.", describe_type(&cat));
+
+	// 3.
+	println!("==> {}.", hello_type::<Human>());
 }
 
+// 1. PhantomData
 struct Labrador {}
 struct Retriever {}
 struct Poodle {}
@@ -39,5 +48,40 @@ impl Dog<Labrador> {
 impl Dog<Retriever> {
 	fn breed_name(&self) -> &str {
 		"retriever"
+	}
+}
+
+// 2. generics and traits
+pub trait SelfDescribing {
+	fn describe(&self) -> String;
+}
+
+fn describe_type<T: SelfDescribing>(t: &T) -> String {
+	t.describe()
+}
+// fn describe_type<T>(t: &T) -> String where T: SelfDescribing { t.describe() }
+
+struct Cat;
+
+impl SelfDescribing for Cat {
+	fn describe(&self) -> String {
+		"curious cat".into() // "happy little dog".into()
+	}
+}
+
+// 3.
+pub trait Hello {
+	fn hello() -> String;
+}
+
+fn hello_type<T: Hello>() -> String {
+	T::hello()
+}
+
+struct Human;
+
+impl Hello for Human {
+	fn hello() -> String {
+		"Hello".into()
 	}
 }
---
+++ 2024_books/Rust_Advanced_Techniques/ch03/c3-1-2/src/main.rs
@@ -0,0 +1,85 @@
+#![allow(dead_code)]
+
+use std::{
+	fs::{create_dir_all, File},
+	io::prelude::*,
+};
+
+fn main() {
+	// println!("Hello, world!");
+
+	match_with_guard(1, false);
+
+	match_enum_types(&DistinctTypes::Name("d2jvkpn".to_string()));
+
+	let black_cat = Cat { name: String::from("Henry"), color: CatColor::Black };
+	let cheshire_cat = Cat { name: String::from("Penelope"), color: CatColor::Cheshire };
+	match_on_black_cats(&black_cat);
+	match_on_black_cats(&cheshire_cat);
+
+	write_to_file().unwrap();
+}
+
+// 1.
+fn match_with_guard(value: i32, choose_first: bool) {
+	match value {
+		v if v == 1 && choose_first => {
+			println!("First match: This value is equal to 1")
+		},
+		v if v == 1 && !choose_first => {
+			println!("Second match: This value is equal to 1")
+		},
+		v if choose_first => {
+			println!("First match: This value is equal to {v}")
+		},
+		v if !choose_first => {
+			println!("Second match: This value is equal to {v}")
+		},
+		_ => println!("Fell through to the default case"),
+	}
+}
+
+// 2.
+enum DistinctTypes {
+	Name(String),
+	Count(i32),
+}
+
+fn match_enum_types(enum_types: &DistinctTypes) {
+	match enum_types {
+		DistinctTypes::Name(name) => println!("name={name}"),
+		DistinctTypes::Count(count) => println!("count={count}"),
+	}
+}
+
+// 3.
+enum CatColor {
+	Black,
+	Red,
+	Chocolate,
+	Cinnamon,
+	Blue,
+	Cream,
+	Cheshire,
+}
+
+struct Cat {
+	name: String,
+	color: CatColor,
+}
+
+fn match_on_black_cats(cat: &Cat) {
+	match cat {
+		Cat { name, color: CatColor::Black } => println!("This is a black cat named {name}"),
+		Cat { name, color: _ } => println!("{name} is not a black cat"),
+	}
+}
+
+// 4.
+fn write_to_file() -> std::io::Result<()> {
+	create_dir_all("data")?;
+
+	let mut file = File::create("data/test01.txt")?;
+	file.write_all(b"File contents\n")?;
+	Ok(())
+}
