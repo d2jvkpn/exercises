7693d1bde644ba416a0245175c70a511e3de6986
d2jvkpn <chenbin2018@protonmail.com>
2024-09-11 09:33:01 +0800

    ...

--- 2024_books/Rust_Advanced_Techniques/ch03/c3-2/src/main.rs
+++ 2024_books/Rust_Advanced_Techniques/ch03/c3-2/src/main.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-use std::{cell::RefCell, fmt, rc::Rc};
+mod linked_list;
+
+use linked_list::LinkedList;
 
 fn main() {
 	// println!("Hello, world!");
@@ -87,7 +89,7 @@ fn main() {
 	}
 
 	numbers.reset_cursor();
-	numbers.head.iter().for_each(|v| println!("--> v: {:?}", v.borrow()));
+	// numbers.head.iter().for_each(|v| println!("--> v: {:?}", v.borrow()));
 
 	numbers.reset_cursor();
 	println!("--> numbers last: {:?}", numbers.last());
@@ -98,91 +100,6 @@ fn main() {
 		person.borrow().hello();
 		// let p = person.into_inner(); // Clone is required for T
 		// p.hello();
-	}
-}
-
-// #[derive(Debug)]
-struct Node<T> {
-	item: Rc<RefCell<T>>,
-	next: NextNode<T>,
-}
-
-type NextNode<T> = Option<Rc<RefCell<Node<T>>>>;
-
-impl<T> Node<T> {
-	pub fn new(item: T) -> Self {
-		Self { item: Rc::new(RefCell::new(item)), next: None }
-	}
-
-	pub fn push(&mut self, item: T) {
-		match &self.next {
-			None => self.next = Node::new(item).into(),
-			Some(v) => v.borrow_mut().push(item),
-		}
-	}
-}
-
-impl<T: fmt::Debug> fmt::Debug for Node<T> {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-		let mut vec = Vec::new();
-		vec.push(self.item.clone());
-
-		let mut next = self.next.clone();
-		while let Some(node) = next {
-			vec.push(node.borrow().item.clone());
-			next = node.borrow().next.clone();
-		}
-
-		write!(f, "{:?}", vec)
-	}
-}
-
-impl<T> From<Node<T>> for NextNode<T> {
-	fn from(node: Node<T>) -> Self {
-		Some(Rc::new(RefCell::new(node)))
-	}
-}
-
-#[derive(Debug)]
-struct LinkedList<T> {
-	head: NextNode<T>,
-	size: usize,
-	cursor: NextNode<T>,
-}
-
-impl<T> LinkedList<T> {
-	fn new(t: T) -> Self {
-		let head: NextNode<T> = Node::new(t).into();
-		Self { head: head.clone(), size: 1, cursor: head.clone() }
-	}
-
-	pub fn push(&mut self, item: T) -> &mut Self {
-		match &self.head {
-			None => self.head = Node::new(item).into(),
-			Some(v) => v.borrow_mut().push(item),
-		};
-
-		self.size += 1;
-		self
-	}
-
-	pub fn size(&self) -> usize {
-		self.size
-	}
-
-	pub fn reset_cursor(&mut self) {
-		self.cursor = self.head.clone();
-	}
-}
-
-impl<T> Iterator for LinkedList<T> {
-	type Item = Rc<RefCell<T>>;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		let cursor = self.cursor.clone()?;
-		let ans = cursor.borrow();
-		self.cursor = ans.next.clone();
-		Some(ans.item.clone())
 	}
 }
 
