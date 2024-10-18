6cc698dcd5d4f0cd9e0a4ff6dfe82b2a0859ca25
d2jvkpn <chenbin2018@protonmail.com>
2024-10-18 18:46:46 +0800

    ...

--- 2024_books/DSAR/ch05/src/priority_queue.rs
+++ 2024_books/DSAR/ch05/src/priority_queue.rs
@@ -1,6 +1,5 @@
-use std::fmt::Debug;
+use std::fmt::{self, Debug};
 
-#[derive(Debug)]
 pub struct Heap<T: PartialOrd + Debug> {
     data: Vec<T>,
     ascending: bool,
@@ -14,14 +13,14 @@ pub fn build_heap<T: PartialOrd>(slice: &mut [T], compare: fn(&T, &T) -> bool) {
 }
 
 pub fn heapify<T: PartialOrd>(slice: &mut [T], index: usize, compare: fn(&T, &T) -> bool) {
-    let left = 2 * index + 1;
-    let right = 2 * index + 2;
     let mut next = index;
 
+    let left = 2 * index + 1;
     if left < slice.len() && !compare(&slice[next], &slice[left]) {
         next = left;
     }
 
+    let right = 2 * index + 2;
     if right < slice.len() && !compare(&slice[next], &slice[right]) {
         next = right;
     }
@@ -53,6 +52,12 @@ impl<T: PartialOrd + Debug> Heap<T> {
         self.data.len() == 0
     }
 
+    #[allow(dead_code)]
+    pub fn ascending(&self) -> bool {
+        self.ascending
+    }
+
+    #[allow(dead_code)]
     fn compare(&self, a: usize, b: usize) -> bool {
         (self.comparator)(&self.data[a], &self.data[b])
     }
@@ -82,19 +87,32 @@ impl<T: PartialOrd + Debug> Heap<T> {
         ans
     }
 
-    pub fn from_vector(items: Vec<T>, ascending: bool) -> Self {
-        let mut heap = Self {
-            data: items,
-            ascending,
-            comparator: if ascending { |a, b| a < b } else { |a, b| a > b },
-        };
+    pub fn min_heap(items: Vec<T>) -> Self {
+        let mut heap = Self { data: items, ascending: true, comparator: |a, b| a < b };
 
         build_heap(&mut heap.data, heap.comparator);
 
         heap
     }
 
-    pub fn into_ordered_vector(mut self) -> Vec<T> {
+    pub fn max_heap(items: Vec<T>) -> Self {
+        let mut heap = Self { data: items, ascending: false, comparator: |a, b| a > b };
+
+        build_heap(&mut heap.data, heap.comparator);
+
+        heap
+    }
+}
+
+impl<T: PartialOrd + Debug> Debug for Heap<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let typ = if self.ascending { "min_heap" } else { "max_heap" };
+        write!(f, "{typ}={:?}", self.data)
+    }
+}
+
+impl<T: PartialOrd + Debug> From<Heap<T>> for Vec<T> {
+    fn from(heap: Heap<T>) -> Vec<T> {
         /*
         let mut ans = Vec::with_capacity(self.size());
 
@@ -104,12 +122,14 @@ impl<T: PartialOrd + Debug> Heap<T> {
 
         ans
         */
-        let high = self.size() - 1;
-        let mut ans = self.data;
+
+        let high = heap.size() - 1;
+        let compare = heap.comparator;
+        let mut ans = heap.data;
 
         for i in (1..=high).rev() {
             ans.swap(0, i);
-            build_heap(&mut ans[..i], self.comparator);
+            build_heap(&mut ans[..i], compare);
         }
 
         ans.reverse();
@@ -123,10 +143,10 @@ mod tests {
 
     #[test]
     fn t_heap() {
-        //
         let nums = vec![9, 7, 5, 11, 12, 2, 14, 3, 10, 6];
 
-        let mut heap = Heap::from_vector(nums.clone(), false);
+        // 1.
+        let mut heap = Heap::max_heap(nums.clone());
         println!("==> heap: {:?}", heap);
 
         assert_eq!(heap.peek(), Some(&14));
@@ -135,16 +155,16 @@ mod tests {
 
         println!("==> heap: {:?}", heap);
 
-        //
-        let heap = Heap::from_vector(nums.clone(), false);
+        // 2.
+        let heap = Heap::max_heap(nums.clone());
 
-        let sorted = vec![14, 12, 11, 10, 9, 7, 6, 5, 3, 2];
-        assert_eq!(heap.into_ordered_vector(), sorted);
+        let output: Vec<_> = heap.into();
+        assert_eq!(output, vec![14, 12, 11, 10, 9, 7, 6, 5, 3, 2]);
 
-        //
-        let heap = Heap::from_vector(nums.clone(), true);
+        // 3.
+        let heap = Heap::min_heap(nums.clone());
 
-        let sorted = vec![2, 3, 5, 6, 7, 9, 10, 11, 12, 14];
-        assert_eq!(heap.into_ordered_vector(), sorted);
+        let output: Vec<_> = heap.into();
+        assert_eq!(output, vec![2, 3, 5, 6, 7, 9, 10, 11, 12, 14]);
     }
 }
