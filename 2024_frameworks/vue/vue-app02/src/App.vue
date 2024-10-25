06402a69bd84c2c09e44753def9c2dc6df82f02e
d2jvkpn <chenbin2018@protonmail.com>
2024-10-25 15:31:42 +0800

    ...

--- 2024_frameworks/vue/vue-app02/src/App.vue
+++ 2024_frameworks/vue/vue-app02/src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
   import MyCountries from './components/MyCountries.vue'
-  import { ref, provide } from "vue";
+  import { ref, provide, onMounted } from "vue";
 
   const name = ref("");
+
+  // onMounted(()=>document.querySelector("input[type=text]").focus());
+  onMounted(() => document.querySelector("input[id=country]").focus());
 
   // 3. watch inject
   provide("name", name);
 </script>
 
 <template>
-  <b>Country</b>: <input type="text" v-model="name" />
+  <b>Country</b>: <input type="text" id="country" v-model="name" />
   <br>
 
   <MyCountries :name="name" />
