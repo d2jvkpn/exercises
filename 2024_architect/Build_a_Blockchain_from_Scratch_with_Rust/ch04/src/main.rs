e2efaa4a14d0b17dca3e5ac329e0cbd9ca47eff8
d2jvkpn <chenbin2018@protonmail.com>
2024-12-09 20:52:21 +0800

    ...

---
+++ 2024_architect/Build_a_Blockchain_from_Scratch_with_Rust/ch04/src/main.rs
@@ -0,0 +1,141 @@
+use support::Dispatch;
+
+mod balances;
+mod support;
+mod system;
+
+fn main() {
+    // println!("Hello, world! WEB3DEV is cool!");
+
+    let mut runtime = Runtime::new();
+
+    let alice = "alice".to_string();
+    let bob = "bob".to_string();
+    let charlie = "charlie".to_string();
+
+    runtime.balances.set_balance(&alice, 100);
+
+    let block_1 = types::Block {
+        header: support::Header { block_number: 1 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::BalancesTransfer { to: bob.clone(), amount: 30 },
+            },
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::BalancesTransfer { to: charlie.clone(), amount: 20 },
+            },
+        ],
+    };
+
+    let block_2 = types::Block {
+        header: support::Header { block_number: 2 },
+        extrinsics: vec![
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::BalancesTransfer { to: bob.clone(), amount: 30 },
+            },
+            support::Extrinsic {
+                caller: alice.clone(),
+                call: RuntimeCall::BalancesTransfer { to: charlie.clone(), amount: 20 },
+            },
+        ],
+    };
+
+    runtime.execute_block(block_1).expect("wrong block execution");
+
+    runtime.execute_block(block_2).expect("wrong block execution");
+
+    /*
+    runtime.balances.set_balance(&alice, 100);
+    runtime.system.inc_block_number();
+
+    assert_eq!(runtime.system.block_number(), 1);
+    runtime.system.inc_nonce(&alice);
+
+    let _ = runtime.balances.transfer(&alice, &bob, 30).map_err(|e| println!("!!! 1. Error: {e}"));
+
+    let _ =
+        runtime.balances.transfer(&alice, &charlie, 20).map_err(|e| println!("!!! 2. Error: {e}"));
+    */
+
+    println!("{:#?}", runtime);
+}
+
+mod types {
+    use crate::support;
+
+    pub type AccountId = String;
+    pub type Balance = u128;
+    pub type BlockNumber = u32;
+    pub type Nonce = u32;
+    pub type Extrinsic = support::Extrinsic<AccountId, crate::RuntimeCall>;
+    pub type Header = support::Header<BlockNumber>;
+    pub type Block = support::Block<Header, Extrinsic>;
+}
+
+pub enum RuntimeCall {
+    BalancesTransfer { to: types::AccountId, amount: types::Balance },
+}
+
+#[derive(Debug)]
+pub struct Runtime {
+    balances: balances::Pallet<Runtime>,
+    system: system::Pallet<Runtime>,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Self { balances: balances::Pallet::new(), system: system::Pallet::new() }
+    }
+
+    fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+        self.system.inc_block_number();
+
+        if self.system.block_number() != block.header.block_number {
+            return Err("Block number mismatch");
+        }
+
+        for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+            self.system.inc_nonce(&caller);
+            // self.dispatch(caller, call)?;
+            if let Err(e) = self.dispatch(caller, call) {
+                eprintln!(
+                    "Extrinsic Error: block_number={}, extrinsic_number={}, error={}",
+                    block.header.block_number, i, e,
+                );
+
+                return Err(e);
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl crate::support::Dispatch for Runtime {
+    type Caller = <Runtime as system::Config>::AccountId;
+    type Call = RuntimeCall;
+
+    fn dispatch(&mut self, caller: Self::Caller, call: Self::Call) -> support::DispatchResult {
+        match call {
+            RuntimeCall::BalancesTransfer { to, amount } => {
+                self.balances.transfer(&caller, &to, amount)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl system::Config for Runtime {
+    type AccountId = types::AccountId;
+    type BlockNumber = types::BlockNumber;
+    type Nonce = types::Nonce;
+}
+
+impl balances::Config for Runtime {
+    // type AccountId = types::AccountId;
+    type Balance = types::Balance;
+}
---
+++ 2024_architect/Build_a_Blockchain_from_Scratch_with_Rust/ch04/src/system.rs
@@ -0,0 +1,99 @@
+use num::traits::{CheckedAdd, CheckedSub, NumAssign, One, Zero};
+
+use std::collections::BTreeMap;
+
+/*
+type AccountId = String;
+type BlockNumber = u32;
+type Nonce = u32;
+*/
+
+pub trait Config {
+    type AccountId: Ord + Clone;
+    type BlockNumber: Copy + Zero + One + CheckedAdd + CheckedSub + NumAssign;
+    type Nonce: Copy + Zero + One + Ord + CheckedAdd;
+}
+
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+    block_number: T::BlockNumber,
+    nonce: BTreeMap<T::AccountId, T::Nonce>,
+}
+
+impl<T: Config> Pallet<T> {
+    pub fn new() -> Self {
+        Self { block_number: T::BlockNumber::zero(), nonce: BTreeMap::new() }
+    }
+
+    pub fn block_number(&self) -> T::BlockNumber {
+        self.block_number
+    }
+
+    pub fn inc_block_number(&mut self) {
+        // unimplemented!()
+        // crash if overflow for purpose
+        self.block_number = self.block_number.checked_add(&T::BlockNumber::one()).unwrap();
+    }
+
+    pub fn inc_nonce(&mut self, who: &T::AccountId) {
+        // let nonce = self.nonce.get(who).unwrap_or(&0);
+        // self.nonce.insert(who.clone(), nonce.checked_add(1).unwrap());
+
+        let value = self.nonce.entry(who.clone()).or_insert(T::Nonce::zero());
+        *value = value.checked_add(&T::Nonce::one()).unwrap();
+    }
+
+    pub fn get_nonce(&self, who: &T::AccountId) -> T::Nonce {
+        *self.nonce.get(&who.clone()).unwrap_or(&T::Nonce::zero())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestConfig;
+
+    impl Config for TestConfig {
+        type AccountId = String;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
+
+    #[test]
+    fn map() {
+        let mut map = BTreeMap::new();
+        let alice = "alice".to_string();
+
+        *map.entry(&alice).or_insert(0) += 1;
+        assert_eq!(map.get(&alice), Some(&1));
+
+        let mut value: u32 = 0;
+        let p = &mut value;
+        value = p.checked_add(1).unwrap_or(0);
+        assert_eq!(value, 1);
+    }
+
+    #[test]
+    fn init_system() {
+        let pallet = Pallet::<TestConfig>::new();
+        assert_eq!(pallet.block_number(), 0);
+    }
+
+    #[test]
+    fn inc_block_number() {
+        let mut pallet = Pallet::<TestConfig>::new();
+        pallet.inc_block_number();
+        assert_eq!(pallet.block_number(), 1);
+    }
+
+    #[test]
+    fn inc_nonce() {
+        let alice = "alice".to_string();
+        let mut pallet = Pallet::<TestConfig>::new();
+
+        pallet.inc_nonce(&alice);
+        pallet.inc_nonce(&alice);
+        assert_eq!(pallet.nonce.get(&alice), Some(&2));
+    }
+}
