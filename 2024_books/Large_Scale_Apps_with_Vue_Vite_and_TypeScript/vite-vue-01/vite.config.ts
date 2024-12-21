3c7f3cefdb022aa603d4104bf94713c80e8540c2
d2jvkpn <chenbin2018@protonmail.com>
2024-12-21 12:17:18 +0800

    ...

--- 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-01/vite.config.ts
+++ 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-01/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+// get --base /site, not --base=/site
 function getArg(key: string): string | undefined {
   const index = process.argv.indexOf(key);
   return index !== -1 ? process.argv[index + 1] : "/";
