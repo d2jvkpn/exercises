12f483d2b91cf32f7923b27e94024d6e3e88bfb6
d2jvkpn <chenbin2018@protonmail.com>
2024-12-23 17:12:18 +0800

    ...

--- 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/do.sh
+++ 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/do.sh
@@ -6,18 +6,18 @@ exit
 npm init vite@latest
 # interactive
 
-npm i -D esbuild@0.24.0
+# npm install -D esbuild@0.24.0
 
 cd vite-vue-ts
 
 npm install
-npm run format
 npm run dev
 
 exit
 npm install vite-tsconfig-paths
 
-npm i --save-dev @types/node
+npm install --save-dev @types/node
 
+exit
 npm update
 npm fund
