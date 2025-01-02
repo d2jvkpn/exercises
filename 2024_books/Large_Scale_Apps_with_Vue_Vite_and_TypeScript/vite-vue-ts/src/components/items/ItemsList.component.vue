500837937bb1a91f3fd65153b8724067f5e3cd0a
d2jvkpn <chenbin2018@protonmail.com>
2025-01-02 17:28:14 +0800

    @/components/items/children

--- 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-ts/src/components/items/ItemsList.component.vue
+++ 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-ts/src/components/items/ItemsList.component.vue
@@ -1,37 +1,63 @@
 <script setup lang="ts">
-  // expose a property called items with a default value of a blank array
-  // defineProps<{ items: any[] }>()
-  // explicetely using any[] as we'll replace this with an interface in the next chapters
+// expose a property called items with a default value of a blank array
+// defineProps<{ items: any[] }>()
+// explicetely using any[] as we'll replace this with an interface in the next chapters
 
-  // import type { ItemInterface } from '@models/items/Item.interface.ts';
-  import type { ItemInterface } from '@/models';
+// import type { ItemInterface } from '@models/items/Item.interface.ts';
+import type { ItemInterface } from '@/models';
 
-  import ItemComponent from './children/Item.component.vue';
-  import Loader from '@/components/shared/Loader.component.vue';
+import ItemComponent from './children/Item.component.vue';
+import Loader from '@/components/shared/Loader.component.vue';
 
-  defineProps<{
-    items: ItemInterface[]
-    loading: boolean
-  }>();
+defineProps<{
+  items: ItemInterface[]
+  loading: boolean
+}>();
 
-  const handleClick = (item: ItemInterface) => {
-    item.selected = !item.selected;
-    console.log(`--> handleItemClick: ${item.id}, ${item.selected}`);
-  };
-
+const handleClick = (item: ItemInterface) => {
+  item.selected = !item.selected;
+  console.log(`--> handleItemClick: ${item.id}, ${item.selected}`);
+};
 </script>
 
+<!--template>
+<div>
+  <h3>Items - loading: {{loading}}:</h3>
+
+  <Loader v-show="loading" />
+  <ul v-show="!loading"> </ul>
+
+  <ul>
+    <li v-for="(item, _index) in items" :key="item.id" @click="handleClick(item)">
+      {{item.name}} [{{item.selected}}]
+    </li>
+  </ul>
+</div>
+</template-->
+
 <template>
-  <div>
-    <h3>Items - loading: {{loading}}:</h3>
+<div>
+  <h3>Items:</h3>
 
-    <Loader v-show="loading" />
-    <ul v-show="!loading"> </ul>
+  <Loader v-show="loading" />
+  <ul v-show="!loading"> </ul>
 
-    <ul>
-      <li v-for="(item, _index) in items" :key="item.id" @click="handleClick(item)">
-        {{item.name}} [{{item.selected}}]
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <ItemComponent v-for="(item, index) in items"
+      :key="item.id"
+      :model="item"
+      @selectItem="handleClick(item)" />
+  </ul>
+</div>
 </template>
+
+<style>
+ul {
+  padding-inline-start: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 0px;
+}
+</style>
