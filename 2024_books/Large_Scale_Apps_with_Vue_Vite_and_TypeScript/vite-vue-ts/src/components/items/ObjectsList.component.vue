bc64f8a17f6b465b5df95c34d1dfc3a40aeb007a
d2jvkpn <chenbin2018@protonmail.com>
2025-01-02 14:35:17 +0800

    ...

--- 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-ts/src/components/items/ObjectsList.component.vue
+++ 2024_books/Large_Scale_Apps_with_Vue_Vite_and_TypeScript/vite-vue-ts/src/components/items/ObjectsList.component.vue
@@ -3,7 +3,7 @@
   // defineProps<{ items: any[] }>()
   // explicetely using any[] as we'll replace this with an interface in the next chapters
 
-  import type { ItemInterface } from '../models/Item.interface.ts';
+  import type { ItemInterface } from '@components/models/Item.interface.ts';
 
   defineProps<{ objects: ItemInterface[] }>();
 
