b4b724fcf718099864b40ee4daa5c8d8ff114564
d2jvkpn <chenbin2018@protonmail.com>
2025-03-03 13:26:45 +0800

    ...

---
+++ 2024_books/Grooking_Deep_Learning/pip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu -o pipefail; _wd=$(pwd); _path=$(dirname $0)
+
+if [ -s  requirements.txt ]; then
+    pip install -r requirements.txt
+else
+    pip install ipython
+    pip install numpy keras tensorflow polars pyyaml
+    pip3 freeze > requirements.txt
+fi
