894672efd0b8449144efedb0d84e1894bfb954fa
d2jvkpn <chenbin2018@protonmail.com>
2025-03-11 02:14:17 +0800

    ...

--- 2025_ai/Grooking_Deep_Learning/ch12/ch12_13.py
+++ 2025_ai/Grooking_Deep_Learning/ch12/ch12_13.py
@@ -11,8 +11,9 @@ with open("data/tasksv11/en/qa1_single-supporting-fact_train.txt", 'r') as f:
     raw = f.readlines()
 
 tokens, vocabs = list(), set()
+
 for line in raw[:1000]:
-    sent = line.lower().replace("\n", "").split()[1:]
+    sent = line.lower().split()[1:]
     tokens.append(sent)
     vocabs.update(set(sent))
 
@@ -32,12 +33,12 @@ def softmax(a):
     return b / b.sum(axis=0)
 
 alpha = 0.001
-iterations = 100 
+iterations = 100
 embed_size = 10
 
-embed = (np.random.rand(len(vocabs) ,embed_size) - 0.5) * 0.1 # word embeddings, range=(-0.05, 0.05)
+embed = (np.random.rand(len(vocabs), embed_size) - 0.5) * 0.1 # word embeddings, range=(-0.05, 0.05)
 recurrent = np.eye(embed_size) # embedding -> embedding (initially the identity matrix)
-start = np.zeros(embed_size)      # sentence embedding for empty sentence
+start = np.zeros(embed_size)   # sentence embedding for empty sentence
 decoder = (np.random.rand(embed_size, len(vocabs)) - 0.5) * 0.1 # embedding -> output weights
 one_hot = np.eye(len(vocabs)) # one hot lookups (for loss function)
 
@@ -46,53 +47,64 @@ def predict(sent):
     loss = 0
     preds = list() # forward propagate
 
-    for i in range(len(sent)):
+    for widx in sent:
+        #print(layers)
         layer = {
-            'pred': softmax(layers[-1]['hidden'].dot(decoder)), # try to predict the next term
-            "hidden": layers[-1]['hidden'].dot(recurrent) + embed[sent[i]] # generate the next hidden state
+            # try to predict the next term
+            "pred": softmax(layers[-1]['hidden'].dot(decoder)), # shape=(embed_size, len(vocabs))
+            # generate the next hidden state
+            "hidden": layers[-1]['hidden'].dot(recurrent) + embed[widx], # shape=(embed_size,)
         }
 
-        #if np.isnan(layer['pred']).any(): break
-        loss += -np.log(layer['pred'][sent[i]])
+        # TODO: RuntimeWarning: invalid value encountered in add
+        loss += -np.log(layer['pred'][widx])
         layers.append(layer)
 
     return layers, loss
 
 # forward
 steps = len(tokens) * iterations
+
 for n in range(steps):
     sent = words2indices(tokens[n%len(tokens)])
     size = float(len(sent))
     layers, loss = predict(sent)
 
     # back propagate
-    for i in reversed(range(len(layers))):
+    for i in reversed(range(1, len(layers))):
         layer = layers[i]
 
-        if(i > 0):
-            target = sent[i-1]
-            layer['output_delta'] = layer['pred'] - one_hot[target]
-            new_hidden_delta = layer['output_delta'].dot(decoder.transpose())
+        layer['output_delta'] = layer['pred'] - one_hot[sent[i-1]] # shape=(embed_size, len(vocabs))
 
-            # if the last layer - don't pull from a later one becasue it doesn't exist
-            if(i == len(layers)-1):
-                layer['hidden_delta'] = new_hidden_delta
-            else:
-                layer['hidden_delta'] = new_hidden_delta + layers[i+1]['hidden_delta'].dot(recurrent.transpose())
-        else:
-            layer['hidden_delta'] = layers[i+1]['hidden_delta'].dot(recurrent.transpose())
+        # if the last layer - don't pull from a later one becasue it doesn't exist
+        layer['hidden_delta'] = layer['output_delta'].dot(decoder.T) # shape=(embed_size, embed_size)
+        if i < len(layers)-1:
+            layer['hidden_delta'] += layers[i+1]['hidden_delta'].dot(recurrent.T)
+
+    layer = layers[0]
+    layer['hidden_delta'] = layers[1]['hidden_delta'].dot(recurrent.T)
+
 
     # update weights
-    #if np.isnan(layers[0]['hidden_delta']).any(): break
-    start -= layers[0]['hidden_delta'] * alpha / size
+    start -= layers[0]['hidden_delta'] / size * alpha
 
-    for i, layer in enumerate(layers[1:]):
-        prev = layers[i]
-        decoder -= np.outer(prev['hidden'], layer['output_delta']) * alpha / size
-        embed[sent[i]] -= prev['hidden_delta'] * alpha / size
-        recurrent -= np.outer(prev['hidden'], layer['hidden_delta']) * alpha / size
+    for i in range(len(layers)-1):
+        layer, layer_next = layers[i], layers[i+1]
+        decoder -= np.outer(layer['hidden'], layer_next['output_delta']) / size * alpha
+        embed[sent[i]] -= layer['hidden_delta'] * alpha / size
+        recurrent -= np.outer(layer['hidden'], layer_next['hidden_delta']) / size * alpha
 
     n+=1
-    if n % 1000 == 0 or n == steps:
+    if n % len(tokens) == 0:
         perplexity = np.exp(loss/size)
-        print(f"--> I{n:05d}: perplexity={perplexity:.3f}")
+        print(f"--> I{n:06d}: perplexity={perplexity:.3f}")
+
+sent_index = 4
+layers, loss = predict(words2indices(tokens[sent_index]))
+print(tokens[sent_index])
+
+for i, layer in enumerate(layers[1:-1]):
+    input = tokens[sent_index][i]
+    target = tokens[sent_index][i+1]
+    pred = vocabs[layer["pred"].argmax()]
+    print(f"Prev input={input}, target={target}, pred={pred}")
