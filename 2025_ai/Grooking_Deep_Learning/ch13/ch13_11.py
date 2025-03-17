ee9d8b37573c0813069f3b48604c0b21c934e31d
d2jvkpn <chenbin2018@protonmail.com>
2025-03-17 17:57:57 +0800

    ...

--- 2025_ai/Grooking_Deep_Learning/ch13/ch13_11.py
+++ 2025_ai/Grooking_Deep_Learning/ch13/ch13_11.py
@@ -8,19 +8,28 @@ np.random.seed(1)
 
 
 data = Tensor([[0, 0], [0, 1], [1, 0], [1, 1]], autograd=True)
-
 target = Tensor([[0], [1], [0], [1]], autograd=True)
 
-wts = list()
-wts.append(Tensor(np.random.rand(2, 3), autograd=True))
-wts.append(Tensor(np.random.rand(3, 1), autograd=True))
+wts = [
+  # input layer -> hidden layer
+  Tensor(np.random.rand(2, 3), autograd=True),
+  # hidden layer -> predication layer
+  Tensor(np.random.rand(3, 1), autograd=True),
+]
 
 optim = SGD(parameters=wts, alpha=0.1)
 
-for i in range(50):
+for n in range(20):
+    n += 1
+
     pred = data.mm(wts[0]).mm(wts[1])
+    if np.isinf(pred.data).any():
+        break
+
     loss = ((pred - target) * (pred - target)).sum(0)
-    print(f"==> loss: {loss.data}")
+    print(f"==> I{n:04d}: loss={loss.data[0]}")
+    # np.isnan(loss.data[0]):
+
     loss.backward(Tensor(np.ones_like(loss.data)))
     optim.step()
 
