6af751a2e5d89ba8475c2bd2ddffabb03725e54b
d2jvkpn <chenbin2018@protonmail.com>
2025-03-21 09:28:40 +0800

    ...

--- 2025_ai/Grooking_Deep_Learning/ch03/s03.py
+++ 2025_ai/Grooking_Deep_Learning/ch03/s03.py
@@ -6,11 +6,10 @@ def neural_network(d, weight):
     return predication
 
 
-weight = 0.1
-
 number_of_toes = [8.5, 9.5, 10, 9]
 d = number_of_toes[0]
 
+weight = 0.1
 pred = neural_network(d, weight)
 
-print(pred)
+print(f"{pred:.3f}")
