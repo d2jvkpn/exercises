564ac143e0bcb91b8a935491966ed82421d60dac
d2jvkpn <chenbin2018@protonmail.com>
2025-05-04 09:41:29 +0800

    Login

--- 2025_frameworks/vue/03.hello-vue/src/components/Login.vue
+++ 2025_frameworks/vue/03.hello-vue/src/components/Login.vue
@@ -1,28 +1,53 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+console.log(`==> login page: VITE_API_URL=${import.meta.env.VITE_API_URL}`);
+
+const loginForm = ref({ account: '', password: ''});
+const errorMsg = ref('');
+const isSubmitting = ref(false);
+
+const submitLogin = async () => {
+  isSubmitting.value = true;
+
+  try {
+    console.log(`--> account login: ${loginForm.value.account}`); // JSON.stringify(loginForm.value)
+    // await new Promise(resolve => setTimeout(resolve, 1000));
+    throw new Error('This is a test error.');
+  } catch (err) {
+    // console.log(`!!! ${err}`);
+    errorMsg.value = err.message || 'Login Failed';
+  } finally {
+    isSubmitting.value = false;
+  }
+}
 </script>
 
-<template>
-  <!--div
-    class="flex items-center justify-center gap-8 min-h-screen bg-gradient-to-br from-green-500 to-sky-400"
-  >
-    <p> Hello, world!</p>
-  </div-->
 
-  <div class="login-page bg-gradient-to-br from-green-500 to-sky-400">
-    <div class="login-container">
-      <span> Account </span>
-      <input
-        type="text" id="account" name="account" required minlength="4" maxlength="32" size="10"
-        placeholder="phone or email"
-      />
-      <span> Password </span>
-      <input
-        type="password" id="password" name="password" required minlength="8" maxlength="32" size="10"
-        placeholder="password"
-      />
-      <button class="login-button"> Login </button>
-    </div>
-  </div>
+<template>
+<div class="login-page bg-gradient-to-br from-green-500 to-sky-400">
+  <form class="login-form" @submit.prevent="submitLogin">
+    <span> Account </span>
+    <input
+      type="text" id="account" name="account" required minlength="6" maxlength="32" size="10"
+      placeholder="phone or email" v-model="loginForm.account"
+    />
+
+    <span> Password </span>
+    <input
+      type="password" id="password" name="password" required minlength="8" maxlength="32" size="10"
+      placeholder="password" v-model="loginForm.password"
+    />
+
+    <p v-if="errorMsg" class="login-error-msg">
+     {{ errorMsg }}
+    </p>
+
+    <button type="submit" :disabled="isSubmitting">
+      {{ isSubmitting ? 'Logging...' : 'Login' }}
+    </button>
+  </form>
+</div>
 </template>
 
 
@@ -35,8 +60,8 @@
   background-color: white;
 }
 
-.login-container {
-  border: 1px solid grey;
+.login-form {
+  border: 1px solid black;
   border-radius: 5px;
   padding: 2rem 2rem;
   margin: 1rem 1rem;
@@ -46,25 +71,31 @@
   grid-template-columns: 1fr 3fr;
   /*justify-content: center;*/
   align-items: center;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
 }
 
-.login-container > input {
+.login-form > span {
+  text-align: right;
+}
+
+.login-form > input {
   border: 1px solid blue;
   padding: 0.5rem;
   min-width: 15rem;
 }
 
-.login-button {
-  background-color: green;
-  border-radius: 5px;
-  padding: 0.2rem;
-  font-size: 1.8rem;
-  color: white;
+.login-error-msg {
+  font-size: 1rem;
+  color: tomato;
   grid-column: 1 / span 2;
 }
 
-.login-container > span {
-  text-align: right;
+.login-form > button {
+  background-color: green;
+  border-radius: 5px;
+  padding: 0.2rem;
+  font-size: 1.5rem;
+  color: white;
+  grid-column: 1 / span 2;
 }
 </style>
