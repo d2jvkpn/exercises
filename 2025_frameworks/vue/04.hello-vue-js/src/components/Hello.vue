603aff0d79d00fdaf73c1162782af75a7e81e123
d2jvkpn <chenbin2018@protonmail.com>
2025-05-09 14:59:56 +0800

    ...

--- 2025_frameworks/vue/04.hello-vue-js/src/components/Hello.vue
+++ 2025_frameworks/vue/04.hello-vue-js/src/components/Hello.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div
-    class="flex items-center justify-center gap-8 min-h-screen bg-gradient-to-br from-green-500 to-sky-400"
-  >
-    <p>Hello, world!</p>
-  </div>
+<div
+  class="flex items-center justify-center gap-8 min-h-screen bg-gradient-to-br from-green-500 to-sky-400"
+>
+  <p>Hello, world!</p>
+</div>
 </template>
