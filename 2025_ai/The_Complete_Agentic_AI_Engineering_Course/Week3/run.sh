6a54761ecebda5bd80d2efbdfb379c542554cce8
d2jvkpn <chenbin2018@protonmail.com>
2025-10-09 19:57:42 +0800

    modified crewai/debate

--- 2025_ai/The_Complete_Agentic_AI_Engineering_Course/Week3/run.sh
+++ 2025_ai/The_Complete_Agentic_AI_Engineering_Course/Week3/run.sh
@@ -2,12 +2,10 @@
 set -eu -o pipefail; _wd=$(pwd); _dir=$(readlink -f `dirname "$0"`)
 
 
-####
+#### 1. create crew debat
 pip install crewai
-
 # uv tool install crewai
 
-####
 crewai create crew debate
 # interactive
 # - Select a provider to set up:
@@ -15,6 +13,7 @@ crewai create crew debate
 # - Select a model to use for Openai:
 # - Enter your OPENAI API key (press Enter to skip):
 
+#### 2. uv install addtional packages
 cd debate
 
 crewai uv sync
@@ -22,8 +21,11 @@ crewai uv sync
 #uv pip install -U httpx litellm
 #uv pip install "requests[socks]" "httpx[socks]"
 
-uv lock --upgrade-package litellm
+source .venv/bin/activate
+
+#uv lock --upgrade-package litellm
 uv add requests[socks] httpx[socks]
-
-
 uv sync
+
+#### 3. run
+crewai run
